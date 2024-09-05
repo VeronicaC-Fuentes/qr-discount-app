0b03e72339ce3766c8cbf14583a4fb659fa26f29
VeronicaC-Fuentes <crucessveronica@gmail.com>
2024-09-05 00:56:22 -0500

    Agregar mejoras

--- src/QRPage.js
+++ src/QRPage.js
@@ -7,18 +7,23 @@ function QRPage() {
 
   useEffect(() => {
     const discountCode = `DISCOUNT-${Math.random().toString(36).substr(2, 9).toUpperCase()}`;
-    const qrLink = `https://tuevento.netlify.app/descuento?code=${discountCode}`; // Asegúrate de usar la URL correcta
+    const qrLink = `https://tuevento.netlify.app/descuento?code=${discountCode}`; 
     setGeneratedQR(qrLink);
   }, []);
 
   return (
-    <div>
-      <h1>Escanea este código QR para obtener tu descuento</h1>
-      {generatedQR && <QRCode value={generatedQR} />}
+    <div className="qr-container">
+      <div className="qr-card">
+        <h1>¡Escanea para obtener tu descuento!</h1>
+        {generatedQR && (
+          <div className="qr-code-container">
+            <QRCode value={generatedQR} size={256} />
+          </div>
+        )}
+        <p className="qr-description">Escanea el código con tu teléfono y sigue los pasos para registrarte.</p>
+      </div>
     </div>
   );
 }
 
 export default QRPage;
-
-
